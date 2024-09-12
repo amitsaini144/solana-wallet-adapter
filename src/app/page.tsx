fc823ea042a5e9123a484c8040bfc27e1e810e6a
amitsaini144 <1amit24saini5@gmail.com>
2024-09-12 19:47:24 +0530

    get balance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,42 @@
 
 import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Spinner } from "@nextui-org/react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { Toaster, toast } from 'sonner'
+import { Toaster, toast } from 'sonner';
+import GetBalance from "@/components/GetBalance";
 
 export default function Home() {
   const { connected, publicKey } = useWallet();
   const { connection } = useConnection();
+  const [amount, setAmount] = useState<number | null>(null);
   const [isClient, setIsClient] = useState(false);
+
+  const getAirdropOnClick = useCallback(async () => {
+    if (!publicKey) {
+      toast.error("Wallet not connected");
+      return;
+    };
+
+    if (!amount) {
+      toast.error("Amount not specified");
+      return;
+    }
+
+    const airdropPromise = async () => {
+      try {
+        await connection.requestAirdrop(publicKey, amount * LAMPORTS_PER_SOL);
+        return "Airdrop confirmed!";
+      } catch (err) {
+        toast.error("Airdrop failed!");
+      }
+    }
+    toast.promise(airdropPromise, {
+      loading: 'Loading...',
+      success: (data) => data,
+    });
+  }, [publicKey, connection]);
 
   useEffect(() => {
     setIsClient(true);
@@ -20,24 +47,6 @@ export default function Home() {
     return <div className="flex items-center justify-center min-h-screen"><Spinner color="secondary" /></div>;
   }
 
-  const getAirdropOnClick = async () => {
-    if (!publicKey) throw new Error("Wallet is not Connected");
-
-    const airdropPromise = async () => {
-      try {
-        await connection.requestAirdrop(publicKey, 1 * LAMPORTS_PER_SOL);
-        return "Airdrop confirmed!";
-      } catch (err) {
-        toast.error("Airdrop failed!");
-      }
-    }
-
-    toast.promise(airdropPromise, {
-      loading: 'Loading...',
-      success: (data) => data,
-    });
-  };
-
   return (
     <main className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col items-center gap-2 rounded">
@@ -46,7 +55,22 @@ export default function Home() {
         {publicKey && (
           <div className="flex flex-col items-center gap-4">
             <h1><span className="font-bold">Your Public key:</span> {publicKey?.toString()}</h1>
+            <GetBalance />
             <div>
+              <input
+                onChange={(e) => {
+                  const value = Number(e.target.value);
+                  if (value > 0) {
+                    setAmount(value);
+                  } else {
+                    setAmount(null);
+                  }
+                }}
+                type="number"
+                min={0}
+                placeholder="Amount"
+                className="border border-gray-300 focus:outline-none font-medium rounded-lg text-sm pl-5 pr-2 py-2.5 me-2 mb-2"
+              />
               <button
                 onClick={getAirdropOnClick}
                 type="button"
---
+++ src/components/GetBalance.tsx
@@ -0,0 +1,38 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from 'sonner';
+
+
+export default function GetBalance() {
+    const { publicKey } = useWallet();
+    const { connection } = useConnection();
+    const [balance, setBalance] = useState<number | null>(null);
+
+    const getBalance = useCallback(async () => {
+        if (!publicKey) {
+            setBalance(null);
+            return;
+        }
+
+        try {
+            const balance = await connection.getBalance(publicKey)
+            setBalance(balance)
+        } catch (error) {
+            toast.error('Failed to fetch balance')
+        }
+    }, [publicKey, connection]);
+
+    useEffect(() => {
+        getBalance();
+    }, []);
+
+    return (
+        <div className="flex items-center gap-4">
+            <h1><span className="font-bold">Your Balance:</span> {balance}</h1>
+            <button onClick={getBalance} type="button" className="text-white bg-[#512da8] border border-gray-300 focus:outline-none hover:bg-black/90 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                Get Balance </button>
+            
+        </div>
+    );
+}
